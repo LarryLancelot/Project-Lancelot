5853f10a448eec99a12eb68561c5d2e0519e049b
LarryLancelot <cjayward63@gmail.com>
2016-12-15 18:00:23 +1100

    Removed HUD
    Removed the old HUD to allow for the new one.

--- Altis_Life.Altis/dialog/statusBar.hpp
+++ Altis_Life.Altis/dialog/statusBar.hpp
@@ -19,14 +19,14 @@ class osefStatusBar {
             w = 1;
             h = 0.03;
             shadow = 1;
-            colorBackground[] = { 1, 0.3, 0, 0.0 };  // uncomment and increase 4th number to have a background
+            colorBackground[] = { 1, 0.3, 0, 0.35 };  // uncomment and increase 4th number to have a background
             font = "PuristaSemibold";
             size = 0.035;
             type = 13;
             style = 1;
             text="Loading server info...";
             class Attributes {
-                align="right";
+                align="center";
                 color = "#FFFFF";
             };
         };
