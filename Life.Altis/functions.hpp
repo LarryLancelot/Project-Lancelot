8763d217b23af0dfb0ee437f51453e20e6f9cb57
LarryLancelot <LarryLancelot@users.noreply.github.com>
2020-08-17 14:07:54 +1000

    funcset

---
+++ Life.Altis/functions.hpp
@@ -0,0 +1,14 @@
+class Server {
+	tag = "SVR";
+	class Database {
+		file = "functions\server";
+		class reqdata{};
+	};
+};
+class Life {
+	tag = "life";
+	class top {
+		file = "functions";
+		test{};
+	};
+};
